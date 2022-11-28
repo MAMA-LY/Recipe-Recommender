d27e78f44244e76e13652f4f03cbf6fef2a2e9ce
AbdelrahmanMosly <abdelrahman.b.mosly@gmail.com>
2022-11-28 09:44:09 +0200

    database population spring initalization

---
+++ Pobulate DB/BrainFood/BrainFood/src/main/java/MAMALY/BrainFood/BrainFoodApplication.java
@@ -0,0 +1,13 @@
+package MAMALY.BrainFood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainFoodApplication.class, args);
+	}
+
+}
