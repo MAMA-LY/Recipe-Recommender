58395122368474d76f9ed9e7c0d24f959f69b206
AbdelrahmanMosly <abdelrahman.b.mosly@gmail.com>
2022-11-28 10:25:54 +0200

    database classes added
    just dummy classes needed for DB population

---
+++ Populate DB/BrainFood/BrainFood/src/main/java/MAMALY/Database Objects/Nutritions.java
@@ -0,0 +1,94 @@
+package SWE Project;
+
+
+/**
+* @generated
+*/
+public class Nutritions {
+    
+    /**
+    * @generated
+    */
+    private int calories;
+    
+    /**
+    * @generated
+    */
+    private int proteins;
+    
+    /**
+    * @generated
+    */
+    private int carbs;
+    
+    /**
+    * @generated
+    */
+    private int fats;
+    
+    
+    
+
+    /**
+    * @generated
+    */
+    public int getCalories() {
+        return this.calories;
+    }
+    
+    /**
+    * @generated
+    */
+    public int setCalories(Integer calories) {
+        this.calories = calories;
+    }
+    
+    
+    /**
+    * @generated
+    */
+    public int getProteins() {
+        return this.proteins;
+    }
+    
+    /**
+    * @generated
+    */
+    public int setProteins(Integer proteins) {
+        this.proteins = proteins;
+    }
+    
+    
+    /**
+    * @generated
+    */
+    public int getCarbs() {
+        return this.carbs;
+    }
+    
+    /**
+    * @generated
+    */
+    public int setCarbs(Integer carbs) {
+        this.carbs = carbs;
+    }
+    
+    
+    /**
+    * @generated
+    */
+    public int getFats() {
+        return this.fats;
+    }
+    
+    /**
+    * @generated
+    */
+    public int setFats(Integer fats) {
+        this.fats = fats;
+    }
+    
+    
+    
+    
+}
