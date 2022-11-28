8e8fc3ceb37388a11a1a1843857e87179a1af61c
AbdelrahmanMosly <95633556+AbdelrahmanMosly@users.noreply.github.com>
2022-11-28 19:15:19 +0200

    Populate db initialization (#8)

---
+++ Populate DB/BrainFood/BrainFood/src/test/java/MAMALY/BrainFood/BrainFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package MAMALY.BrainFood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
