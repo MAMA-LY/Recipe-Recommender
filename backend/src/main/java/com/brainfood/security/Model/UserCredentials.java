b5ee8eaf531536aea77494c18444441cab6c60e2
Muhammad Ibrahim Elkotb <67494587+MuhammadElkotb@users.noreply.github.com>
2022-12-03 19:45:26 +0200

    User auth is Done (#16)
    All comments are addressed and resolved and merge was successful

---
+++ backend/src/main/java/com/brainfood/security/Model/UserCredentials.java
@@ -0,0 +1,37 @@
+package com.brainfood.security.Model;
+
+import java.io.Serializable;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.ToString;
+
+
+@Component
+@ToString
+@Entity
+@Table(name = "\"UserCredentials\"")
+@Data
+public class UserCredentials implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String ID;
+
+    @Column(name = "username")
+    private String username;
+  
+    @Column(name = "password")
+    private String password;
+ 
+    @Column(name = "email")
+    private String email;
+    
+}
