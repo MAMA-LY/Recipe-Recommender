432c160194aecfcce083045b464395ff2de1574e
AbdelrahmanMosly <95633556+AbdelrahmanMosly@users.noreply.github.com>
2022-12-05 00:44:25 +0200

    Bf 30 bug fixed (#18)
    Co-authored-by: Deffo0 <73228199+Deffo0@users.noreply.github.com>

--- PopulateDatabase/src/main/java/com/BrainFood/APIsClients/CalorieNinjasClient.java
+++ PopulateDatabase/src/main/java/com/BrainFood/APIsClients/CalorieNinjasClient.java
@@ -2,22 +2,27 @@ package com.BrainFood.APIsClients;
 
 import org.json.JSONObject;
 
-import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class CalorieNinjasClient {
-    public static JSONObject getNutrition(String food) throws IOException, InterruptedException {
-        food=food.replaceAll(" ","%20"); //api use %20 for space
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://calorieninjas.p.rapidapi.com/v1/nutrition?query="+food))
-                .header("X-RapidAPI-Key", "2179e35da2mshf75f9d6bd9fca69p169778jsn475f5672c929")
-                .header("X-RapidAPI-Host", "calorieninjas.p.rapidapi.com")
-                .method("GET", HttpRequest.BodyPublishers.noBody())
-                .build();
-        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-        return new JSONObject(response.body());
+    public static JSONObject getNutrition(String food) {
+        try {
+            food=food.replaceAll("[ ()]","%20"); //api use %20 for space
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://calorieninjas.p.rapidapi.com/v1/nutrition?query=" + food))
+                    .header("X-RapidAPI-Key", "2179e35da2mshf75f9d6bd9fca69p169778jsn475f5672c929")
+                    .header("X-RapidAPI-Host", "calorieninjas.p.rapidapi.com")
+                    .method("GET", HttpRequest.BodyPublishers.noBody())
+                    .build();
+            HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+            return new JSONObject(response.body());
+        }
+        catch (Exception e)
+        {
+            return new JSONObject();
+        }
     }
 }
---
+++ PopulateDatabase/src/main/java/com/BrainFood/Collectors/IngredientCollector.java
@@ -0,0 +1,33 @@
+package com.BrainFood.Collectors;
+
+import com.BrainFood.APIsClients.CalorieNinjasClient;
+import com.BrainFood.DatabaseEntities.Ingredient;
+import com.spoonacular.client.model.GetRecipeInformation200ResponseExtendedIngredientsInner;
+import lombok.NoArgsConstructor;
+import org.json.JSONObject;
+import java.io.IOException;
+
+@NoArgsConstructor
+public class IngredientCollector {
+
+    private final CollectorFitters collectorFitters = new CollectorFitters();
+
+    public Ingredient collect(GetRecipeInformation200ResponseExtendedIngredientsInner ingredientsInner)
+            throws IOException, InterruptedException {
+
+        JSONObject ingredientNutrition = CalorieNinjasClient.getNutrition(ingredientsInner.getOriginal());
+        if(ingredientNutrition.toString().equals("{}")
+                || ingredientNutrition.getJSONArray("items")==null
+                || ingredientNutrition.getJSONArray("items").length()==0) {
+            return null;
+        }
+        Ingredient ingredient = Ingredient.builder()
+                                .name(ingredientsInner.getName())
+                                .amount(ingredientsInner.getOriginal())
+                                .build();
+
+        collectorFitters.ingredientNutritionFitter(ingredient, ingredientNutrition);
+        return ingredient;
+    }
+
+}
