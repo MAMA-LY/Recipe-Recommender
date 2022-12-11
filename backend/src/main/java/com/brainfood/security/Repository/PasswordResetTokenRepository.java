5e81c6b0a4c7dbe36d418f123523a2d89a7c40d5
AbdelrahmanMosly <abdelrahman.b.mosly@gmail.com>
2022-12-11 19:22:02 +0200

    BF-37 remove @data from JPA to getters and setters

--- backend/src/main/java/com/brainfood/security/Repository/PasswordResetTokenRepository.java
+++ backend/src/main/java/com/brainfood/security/Repository/PasswordResetTokenRepository.java
@@ -1,7 +1,5 @@
 package com.brainfood.security.Repository;
 
-import java.util.UUID;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
