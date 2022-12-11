f76fc2cc274290d7566dcad45205c8c3872d8fae
AbdelrahmanMosly <abdelrahman.b.mosly@gmail.com>
2022-12-12 00:00:16 +0200

    BF-43 Ingredients unit test

---
+++ backend/src/test/java/com/brainfood/backend/IngredientTest.java
@@ -0,0 +1,187 @@
+package com.brainfood.backend;
+
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+import java.awt.image.ImageObserver;
+import java.awt.image.ImageProducer;
+import java.lang.reflect.Field;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IngredientTest {
+
+    @Test
+    void getName() throws IllegalAccessException, NoSuchFieldException{
+        final Ingredient ingredient =new Ingredient();
+        final Field field = ingredient.getClass().getDeclaredField("name");
+        field.setAccessible(true);
+        String expectedValue = "name1";
+        field.set(ingredient, expectedValue);
+
+        assertEquals(expectedValue,ingredient.getName(), "problem in getName");
+
+    }
+
+    @Test
+    void getID() throws IllegalAccessException, NoSuchFieldException{
+
+        final Ingredient ingredient =new Ingredient();
+        final Field field = ingredient.getClass().getDeclaredField("ID");
+        field.setAccessible(true);
+        String expectedValue = "id1";
+        field.set(ingredient, expectedValue);
+
+        assertEquals(expectedValue,ingredient.getID(), "problem in getID");
+    }
+
+    @Test
+    void getIcon() throws IllegalAccessException, NoSuchFieldException{
+
+        final Ingredient ingredient =new Ingredient();
+        final Field field = ingredient.getClass().getDeclaredField("icon");
+        field.setAccessible(true);
+        Image expectedValue = new Image() {
+            @Override
+            public int getWidth(ImageObserver observer) {
+                return 0;
+            }
+
+            @Override
+            public int getHeight(ImageObserver observer) {
+                return 0;
+            }
+
+            @Override
+            public ImageProducer getSource() {
+                return null;
+            }
+
+            @Override
+            public Graphics getGraphics() {
+                return null;
+            }
+
+            @Override
+            public Object getProperty(String name, ImageObserver observer) {
+                return null;
+            }
+        };
+        field.set(ingredient, expectedValue);
+
+        assertEquals(expectedValue,ingredient.getIcon(), "problem in getIcon");
+    }
+
+    @Test
+    void getWeight() throws IllegalAccessException, NoSuchFieldException{
+
+
+        final Ingredient ingredient =new Ingredient();
+        final Field field = ingredient.getClass().getDeclaredField("weight");
+        field.setAccessible(true);
+        int expectedValue = 5;
+        field.set(ingredient, expectedValue);
+
+        assertEquals(expectedValue,ingredient.getWeight(), "problem in getWeight");
+    }
+
+    @Test
+    void getNutritions() throws IllegalAccessException, NoSuchFieldException{
+
+
+        final Ingredient ingredient =new Ingredient();
+        final Field field = ingredient.getClass().getDeclaredField("nutritions");
+        field.setAccessible(true);
+        Nutritions expectedValue = new Nutritions();
+        field.set(ingredient, expectedValue);
+
+        assertEquals(expectedValue,ingredient.getNutritions(), "problem in getNutritions");
+    }
+
+    @Test
+    void setName() throws IllegalAccessException, NoSuchFieldException{
+
+        final Ingredient ingredient =new Ingredient();
+        final Field field = ingredient.getClass().getDeclaredField("name");
+        field.setAccessible(true);
+        String expectedValue = "name2";
+
+        ingredient.setName(expectedValue);
+        String actualValue= (String)field.get(ingredient);
+        assertEquals(expectedValue,actualValue,"problem in setName");
+
+    }
+
+    @Test
+    void setID() throws IllegalAccessException, NoSuchFieldException{
+        final Ingredient ingredient =new Ingredient();
+        final Field field = ingredient.getClass().getDeclaredField("ID");
+        field.setAccessible(true);
+        String expectedValue = "id2";
+
+        ingredient.setID(expectedValue);
+        String actualValue= (String)field.get(ingredient);
+        assertEquals(expectedValue,actualValue,"problem in setID");
+    }
+
+    @Test
+    void setIcon() throws IllegalAccessException, NoSuchFieldException{
+        final Ingredient ingredient =new Ingredient();
+        final Field field = ingredient.getClass().getDeclaredField("icon");
+        field.setAccessible(true);
+        Image expectedValue = new Image() {
+            @Override
+            public int getWidth(ImageObserver observer) {
+                return 0;
+            }
+
+            @Override
+            public int getHeight(ImageObserver observer) {
+                return 0;
+            }
+
+            @Override
+            public ImageProducer getSource() {
+                return null;
+            }
+
+            @Override
+            public Graphics getGraphics() {
+                return null;
+            }
+
+            @Override
+            public Object getProperty(String name, ImageObserver observer) {
+                return null;
+            }
+        };
+
+        ingredient.setIcon(expectedValue);
+        Image actualValue= (Image)field.get(ingredient);
+        assertEquals(expectedValue,actualValue,"problem in setIcon");
+    }
+
+    @Test
+    void setWeight() throws IllegalAccessException, NoSuchFieldException{
+        final Ingredient ingredient =new Ingredient();
+        final Field field = ingredient.getClass().getDeclaredField("weight");
+        field.setAccessible(true);
+        int expectedValue = 101;
+
+        ingredient.setWeight(expectedValue);
+        int actualValue= (int)field.get(ingredient);
+        assertEquals(expectedValue,actualValue,"problem in setWeight");
+    }
+
+    @Test
+    void setNutritions() throws IllegalAccessException, NoSuchFieldException{
+        final Ingredient ingredient =new Ingredient();
+        final Field field = ingredient.getClass().getDeclaredField("nutritions");
+        field.setAccessible(true);
+        Nutritions expectedValue = new Nutritions();
+
+        ingredient.setNutritions(expectedValue);
+        Nutritions actualValue= (Nutritions)field.get(ingredient);
+        assertEquals(expectedValue,actualValue,"problem in setNutritions");
+    }
+}
