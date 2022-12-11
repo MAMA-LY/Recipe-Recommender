50b632f83e3ded9296cde22b24061179a32ab9b9
AbdelrahmanMosly <abdelrahman.b.mosly@gmail.com>
2022-12-11 18:36:04 +0200

    BF-37 Refactoring database object

--- backend/src/main/java/com/brainfood/backend/Database.java
+++ backend/src/main/java/com/brainfood/backend/Database.java
@@ -1,68 +1,35 @@
 
 package com.brainfood.backend;
 
+import lombok.Setter;
+
 import java.util.HashSet;
 import java.util.Set;
 
-/**
-* @generated
-*/
+@Setter
 public class Database {
-    
-    /**
-    * @generated
-    */
-    private Database instance;
-    
-    
-    /**
-    * @generated
-    */
-    private Set<DAO> DAO;
-    
-    
 
-    /**
-    * @generated
-    */
-    // private Database getInstance() {
-    //     return this.instance;
-    // }
-    
-    /**
-    * @generated
-    */
-    private void setInstance(Database instance) {
-        this.instance = instance;
-    }
-    
-    
-    
-    /**
-    * @generated
-    */
+    private Database instance;
+
+    private Set<DAO> DAO;
+
+
+
+    //                          Operations                                  
+
     public Set<DAO> getDAO() {
         if (this.DAO == null) {
             this.DAO = new HashSet<DAO>();
         }
         return this.DAO;
     }
-    
-    /**
-    * @generated
-    */
-    public void setDAO(Set<DAO> DAO) {
-        this.DAO = DAO;
-    }
 
-    //                          Operations                                  
-    
-    /**
-    * @generated
-    */
     public Database getInstance() {
         //
-        
+        if(this.instance==null)
+            return new Database();
+        else
+            return this.instance;
     }
     
     /**
