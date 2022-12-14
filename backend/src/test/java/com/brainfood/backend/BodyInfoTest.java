3cd25c279a92b9b6badfa7482df36ded6a212890
AbdelrahmanMosly <abdelrahman.b.mosly@gmail.com>
2022-12-14 12:04:53 +0200

    BF-43 remove Order in BodyInfoTests class

--- backend/src/test/java/com/brainfood/backend/BodyInfoTest.java
+++ backend/src/test/java/com/brainfood/backend/BodyInfoTest.java
@@ -4,37 +4,41 @@ import org.junit.jupiter.api.*;
 import java.lang.reflect.Field;
 import static org.junit.jupiter.api.Assertions.*;
 
-
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class BodyInfoTest {
 
-    static BodyInfo bodyInfo;
-    @BeforeAll
-    static void setUp() {
-        bodyInfo =new BodyInfo(100,50,12);
+    @Test
+    void getHeight() throws NoSuchFieldException, IllegalAccessException {
+        final BodyInfo bodyInfo =new BodyInfo();
+        final Field field = bodyInfo.getClass().getDeclaredField("height");
+        field.setAccessible(true);
+        int expectedValue = 100;
+        field.set(bodyInfo, expectedValue);
+        assertEquals(expectedValue, bodyInfo.getHeight(), "problem in getHeight");
     }
 
     @Test
-    @Order(1)
-    void getHeight() {
-        assertEquals(100.0,bodyInfo.getHeight(),"getHeight problem");
+    void getWeight() throws NoSuchFieldException, IllegalAccessException {
+        final BodyInfo bodyInfo =new BodyInfo();
+        final Field field = bodyInfo.getClass().getDeclaredField("weight");
+        field.setAccessible(true);
+        int expectedValue = 50;
+        field.set(bodyInfo, expectedValue);
+        assertEquals(expectedValue, bodyInfo.getWeight(), "problem in getWeight");
     }
 
     @Test
-    @Order(2)
-    void getWeight() {
-        assertEquals(50.0,bodyInfo.getWeight(),"getWeight problem");
+    void getAge() throws NoSuchFieldException, IllegalAccessException {
+        final BodyInfo bodyInfo =new BodyInfo();
+        final Field field = bodyInfo.getClass().getDeclaredField("age");
+        field.setAccessible(true);
+        int expectedValue = 12;
+        field.set(bodyInfo, expectedValue);
+        assertEquals(expectedValue, bodyInfo.getAge(), "problem in getAge");
     }
 
     @Test
-    @Order(3)
-    void getAge() {
-        assertEquals(12,bodyInfo.getAge(),"getAge problem");
-    }
-
-    @Test
-    @Order(4)
     void setHeight() throws NoSuchFieldException, IllegalAccessException {
+        final BodyInfo bodyInfo =new BodyInfo();
         bodyInfo.setHeight(120.5f);
         final Field feild=bodyInfo.getClass().getDeclaredField("height");
         feild.setAccessible(true);
@@ -44,8 +48,8 @@ class BodyInfoTest {
     }
 
     @Test
-    @Order(5)
     void setWeight() throws NoSuchFieldException, IllegalAccessException {
+        final BodyInfo bodyInfo =new BodyInfo();
         bodyInfo.setWeight(70.5f);
         final Field feild=bodyInfo.getClass().getDeclaredField("weight");
         feild.setAccessible(true);
@@ -54,8 +58,8 @@ class BodyInfoTest {
     }
 
     @Test
-    @Order(6)
     void setAge() throws NoSuchFieldException, IllegalAccessException {
+        final BodyInfo bodyInfo =new BodyInfo();
         bodyInfo.setAge(15);
         final Field feild=bodyInfo.getClass().getDeclaredField("age");
         feild.setAccessible(true);
