6e9d3765fa72f41d348aea942337eed98a0627f1
Youssef Bazina <youssefbazina7@gmail.com>
2022-12-15 13:21:00 +0200

    Manual Cherry pick

--- frontend/lib/api/home_recipes_api.dart
+++ frontend/lib/api/home_recipes_api.dart
@@ -2,16 +2,32 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:flutter/material.dart';
+import 'package:recipe_recommender_frontend/main.dart';
 import 'package:recipe_recommender_frontend/models/recipe.dart';
 import 'package:http/http.dart' as http;
 
 import '../models/failure.dart';
 
 class GetRecipesAPI {
+  var cookie;
+
+  GetRecipesAPI() {}
+
+  GetRecipesAPI.fromCookie(var this.cookie);
+
   Future<List<Recipe>> getRecipes(String path) async {
-    var url = Uri.https(
-        "${const String.fromEnvironment("BrainFoodBackendIP", defaultValue: "brainfood.azurewebsites.net")}", "/$path");
-    var response = await http.get(url);
+
+    var url = Uri.http(
+        "${const String.fromEnvironment("BrainFoodBackendIP")}:8080", "/$path");
+    var response = await http.get(url, headers: {
+      "cookie": session.cookie,
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "POST, OPTIONS, GET",
+      "Access-Control-Allow-Headers":
+      "Origin,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,locale",
+    });
+    debugPrint(response.statusCode.toString());
 
     if (response.statusCode == 200) {
       return Recipe.recipesFromSnapshot(jsonDecode(response.body));
@@ -22,8 +38,30 @@ class GetRecipesAPI {
           code: response.statusCode, message: response.headers.toString());
     }
   }
-}
 
+  Future<List<Recipe>> getRecipesWithQuery(String path, Map<String, String> query) async {
+
+    var url = Uri.http(
+        "${const String.fromEnvironment("BrainFoodBackendIP")}:8080", "/$path", query);
+    var response = await http.get(url, headers: {
+      "cookie": session.cookie,
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "POST, OPTIONS, GET",
+      "Access-Control-Allow-Headers":
+      "Origin,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,locale",
+    });
+    debugPrint(response.statusCode.toString());
+    debugPrint(response.body.toString());
+    if (response.statusCode == 200) {
+      return Recipe.recipesFromSnapshot(jsonDecode(response.body.toString()));
+    } else if (response.statusCode == 401) {
+      throw Failure(code: 401, message: "fail");
+    } else {
+      throw Failure(
+          code: response.statusCode, message: response.headers.toString());
+    }
+  }
+}
 
 
 
