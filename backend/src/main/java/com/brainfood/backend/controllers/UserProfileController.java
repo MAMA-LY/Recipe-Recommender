063128c6a6fdad8060e0e1829d470883baf6b974
AbdelrahmanMosly <abdelrahman.b.mosly@gmail.com>
2022-12-29 00:11:15 +0200

    Revert "Revert "BF-90 deisgn with real data and tested""
    This reverts commit 1747372d413eaf5e03e833db88c1bff08dbe0681.

--- backend/src/main/java/com/brainfood/backend/controllers/UserProfileController.java
+++ backend/src/main/java/com/brainfood/backend/controllers/UserProfileController.java
@@ -3,10 +3,14 @@ package com.brainfood.backend.controllers;
 import com.brainfood.backend.DAO;
 import com.brainfood.backend.models.UserProfile;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@ComponentScan
+@RestController
 @RequestMapping("account_settings")
 public class UserProfileController {
 
