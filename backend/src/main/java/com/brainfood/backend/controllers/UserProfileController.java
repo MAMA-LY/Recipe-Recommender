4c51e11ee55488d3310b321f569e366aa93007a9
AbdelrahmanMosly <abdelrahman.b.mosly@gmail.com>
2022-12-28 21:03:29 +0200

    BF-86 use real data

--- backend/src/main/java/com/brainfood/backend/controllers/UserProfileController.java
+++ backend/src/main/java/com/brainfood/backend/controllers/UserProfileController.java
@@ -1,6 +1,8 @@
 package com.brainfood.backend.controllers;
 
+import com.brainfood.backend.DAO;
 import com.brainfood.backend.models.UserProfile;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,13 +10,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/account_settings")
 public class UserProfileController {
 
+    @Autowired
+    DAO DAO;
     @GetMapping("/userProfile")
     public UserProfile getUserProfile(){
-        UserProfile userProfile=new UserProfile();
-        String username = SecurityContextHolder.getContext().getAuthentication().getName();
-
-
-        userProfile.builder().username(username).build();
-        return userProfile;
+        return DAO.getUserProfile();
     }
 }
